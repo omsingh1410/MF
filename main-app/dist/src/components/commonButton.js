63a7e9ac67ef3307e4ce6b5289398a14c674cf87
Om Singh <om.singh@digitalspace.co.in>
2020-06-08 00:15:35 +0530

    Initial commit

---
+++ main-app/dist/src/components/commonButton.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _reactstrap = require("reactstrap");
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = function _default(_ref) {
+  var text = _ref.text;
+  return _react["default"].createElement(_reactstrap.Button, {
+    color: "danger"
+  }, text);
+};
+
+exports["default"] = _default;
+
+//# sourceMappingURL=commonButton.js.map
---
+++ main-app/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/commonButton.js";
+
+const navigateTo = url => window.history.pushState(null, null, url);
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pathname: window.location.pathname
+    };
+  }
+
+  handleMenuClick = () => {
+    this.setState(() => ({ pathname: window.location.pathname }));
+  };
+
+  render() {
+    return (
+      <div className="Menu" onClick={this.handleMenuClick}>
+        {this.props.children(this.state.pathname)}
+      </div>
+    );
+  }
+}
+
+const MenuItem = ({ link, children, pathname }) => {
+  const classes = ["Menu-Item"];
+  if (pathname === link) {
+    classes.push("Menu-Item--Selected");
+  }
+  return (
+    <div className={classes.join(" ")} onClick={() => navigateTo(link)}>
+      {children}
+    </div>
+  );
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React 15 Stitching Layer</h1>
+        </header>
+        <div className="App-content">
+          <Menu>
+            {pathname => (
+              <div>
+                <MenuItem pathname={pathname} link="/">
+                  All applications
+                </MenuItem>
+                <MenuItem pathname={pathname} link="/react">
+                  React 16 application
+                </MenuItem>
+                <MenuItem pathname={pathname} link="/angular">
+                  Angular 5 application
+                </MenuItem>
+              </div>
+            )}
+          </Menu>
+          <div className="App-container">
+            <div id="reacttt-app" />
+            <div id="angular-app" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
